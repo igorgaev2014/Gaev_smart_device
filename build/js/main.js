d4cfb9911e062d538edf1d3d17833db1a9864bbb
Игорь Гаев <igorgaev2014@gmail.com>
2022-03-15 20:35:12 +0300

    popup

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,19 @@ const body = document.body;
 const accordions = document.querySelectorAll('.accordion');
 const accordionButtons = document.querySelectorAll('.info-footer__button');
 const accordionLists = document.querySelectorAll('.info-footer__list');
+const form = document.querySelector('#feedback .form');
+const formModal = document.querySelector('.modal .form');
+const nameInput = document.querySelector('#name');
+const telInput = document.querySelector('#tel');
+const textareaInput = document.querySelector('#textarea');
+const nameInputModal = document.querySelector('#name-modal');
+const telInputModal = document.querySelector('#tel-modal');
+const textareaInputModal = document.querySelector('#textarea-modal');
+const overlay = document.querySelector('.overlay');
+const modalOpenButton = document.querySelector('.main-nav__button');
+const modalCloseButton = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal.feedback');
+const nameField = document.querySelector('.modal [type="text"]');
 
 body.classList.remove('no-js');
 
@@ -29,4 +42,68 @@ accordions.forEach(function (accordion) {
     }
   });
 });
+
+if (modalOpenButton) {
+  modalOpenButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    if (modal.classList.contains('modal--closed')) {
+      modal.classList.remove('modal--closed');
+      overlay.classList.remove('overlay--closed');
+      body.classList.add('overflow');
+      nameField.focus();
+    }
+  });
+}
+
+if (modalCloseButton) {
+  modalCloseButton.addEventListener('click', function () {
+    if (!modal.classList.contains('modal--closed')) {
+      modal.classList.add('modal--closed');
+      overlay.classList.add('overlay--closed');
+      body.classList.remove('overflow');
+    }
+  });
+}
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (!modal.classList.contains('modal--closed')) {
+      modal.classList.add('modal--closed');
+      overlay.classList.add('overlay--closed');
+      body.classList.remove('overflow');
+    }
+  }
+});
+
+if (overlay) {
+  overlay.addEventListener('click', function () {
+    if (!modal.classList.contains('modal--closed')) {
+      modal.classList.add('modal--closed');
+      overlay.classList.add('overlay--closed');
+      body.classList.remove('overflow');
+    }
+  });
+}
+
+IMask(document.querySelector('#tel'), {mask: '+{7}(000)000-00-00'});
+IMask(document.querySelector('#tel-modal'), {mask: '+{7}(000)000-00-00'});
+
+if (form) {
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    localStorage.setItem('name', nameInput.value);
+    localStorage.setItem('tel', telInput.value);
+    localStorage.setItem('textarea', textareaInput.value);
+  });
+}
+
+if (formModal) {
+  formModal.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    localStorage.setItem('name-modal', nameInputModal.value);
+    localStorage.setItem('tel-modal', telInputModal.value);
+    localStorage.setItem('textarea-modal', textareaInputModal.value);
+  });
+}
 //# sourceMappingURL=main.js.map
